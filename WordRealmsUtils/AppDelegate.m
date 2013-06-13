3a313fdde7b9d71bf7b62756986a924dd365468a
Dan Dela Rosa <dandelarosa@gmail.com>
2013-06-12 22:40:17 -0400

    Initial Commit

---
+++ WordRealmsUtils/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  WordRealmsUtils
+//
+//  Created by Dan Dela Rosa on 6/12/13.
+//  Copyright (c) 2013 MRD Engineering. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ WordRealmsUtils/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WordRealmsUtils
+//
+//  Created by Dan Dela Rosa on 6/12/13.
+//  Copyright (c) 2013 MRD Engineering. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
